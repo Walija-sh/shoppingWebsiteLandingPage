8220903eaf31e1521ce1ae68063b0e640e725855
Walija-sh <walijash@gmail.com>
2026-01-09 03:20:18 +0500

    updated to hashrouting

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <AppProvider>
 
     <App />
     </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
